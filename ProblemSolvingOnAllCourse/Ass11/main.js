bd82f6fc7ea8c331c157b3d6e5b9e8dc1bd3027c
theeyad <eyadosman35@gmail.com>
2026-02-26 22:28:38 +0200

    Ass11

---
+++ ProblemSolvingOnAllCourse/Ass11/main.js
@@ -0,0 +1,12 @@
+// // Normal
+
+// let myMoney = 5301503206;
+
+// let formated = myMoney.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+// console.log(formated)
+
+// // High
+let myMoney = 5301503206;
+
+console.log(myMoney.toLocaleString())
